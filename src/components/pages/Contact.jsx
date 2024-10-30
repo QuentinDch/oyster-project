34bea9ee47441e960734331454edaf5c75653b3d
Quentin <quentin.caflo@gmail.com>
2024-10-30 16:46:27 +0100

    add informations page

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -4,53 +4,63 @@ const Contact = () => {
   return (
     <section className="contact">
       <Title title="Contact" />
-      <ul className="contact-list">
-        <li className="contact-item">
-          <div className="contact-detail">
-            <h3>phone.</h3>
-            <a
-              href="tel:+441315556789"
-              aria-label="Call us at +44 131 555 6789"
-            >
-              <span className="sr-only">Call us at </span>+44 131 555 6789
-            </a>
-          </div>
-        </li>
-        <li className="contact-item">
-          <div className="contact-detail">
-            <h3>email.</h3>
-            <a href="mailto:hello@oysterdiscovery.co.uk">
-              hello@oysterdiscovery.co.uk
-            </a>
-          </div>
-        </li>
-        <li className="contact-item">
-          <div className="contact-detail">
-            <h3>address.</h3>
-            <address>
-              123 Royal Mile <br />
-              EH1 2QR Edinburgh <br />
-              Scotland
-            </address>
-          </div>
-        </li>
-        <li className="contact-item">
-          <div className="contact-detail">
-            <h3>social.</h3>
-            <div className="contact-socials">
-              <a href="https://www.facebook.com/" target="_blank">
-                facebook
-              </a>
-              <a href="https://www.instagram.com/" target="_blank">
-                instagram
-              </a>
-              <a href="#" target="_blank">
-                gitHub
+      <div className="contact-grid">
+        <div className="contact-text">
+          <p>
+            Feel free to reach out to us through our contact options. We are
+            here to assist you and answer any questions you may have. Our team
+            looks forward to welcoming you and helping you with your
+            reservations.
+          </p>
+        </div>
+        <ul className="contact-list">
+          <li className="contact-item">
+            <div className="contact-detail">
+              <h3>phone.</h3>
+              <a
+                href="tel:+441315556789"
+                aria-label="Call us at +44 131 555 6789"
+              >
+                <span className="sr-only">Call us at </span>+44 131 555 6789
               </a>
             </div>
-          </div>
-        </li>
-      </ul>
+          </li>
+          <li className="contact-item">
+            <div className="contact-detail">
+              <h3>email.</h3>
+              <a href="mailto:hello@oysterdiscovery.co.uk">
+                hello@oysterdiscovery.co.uk
+              </a>
+            </div>
+          </li>
+          <li className="contact-item">
+            <div className="contact-detail">
+              <h3>address.</h3>
+              <address>
+                123 Royal Mile <br />
+                EH1 2QR Edinburgh <br />
+                Scotland
+              </address>
+            </div>
+          </li>
+          <li className="contact-item">
+            <div className="contact-detail">
+              <h3>social.</h3>
+              <div className="contact-socials">
+                <a href="https://www.facebook.com/" target="_blank">
+                  facebook
+                </a>
+                <a href="https://www.instagram.com/" target="_blank">
+                  instagram
+                </a>
+                <a href="#" target="_blank">
+                  gitHub
+                </a>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
     </section>
   );
 };
--- src/components/pages/Information.jsx
+++ src/components/pages/Information.jsx
@@ -1,5 +1,47 @@
+import Title from "../Title";
+
 const Information = () => {
-  return <div>Information</div>;
+  return (
+    <section className="informations">
+      <Title title="Informations" />
+      <div className="informations-details details">
+        <p>
+          The Oyster Restaurant is located in Edinburgh. It offers seasonal
+          cuisine that highlights well-sourced local products.
+        </p>
+        <p>
+          Athanase creates simple dishes that focus on essential flavors and
+          striking flavor pairings, always with a concern for indulgence.
+        </p>
+        <p>
+          The selection of wines is primarily based on encounters with
+          passionate winemakers, followed by our favorites, and finally on
+          suggestions for pairing with dishes that are always present in the
+          back of our minds.
+        </p>
+      </div>
+      <div className="informations-details gallery">
+        <figure className="gallery-item">
+          <div className="gallery-item-wrapper">
+            <img src="/images/kw-bl.jpg" alt="" />
+          </div>
+          <figcaption>welcomming committee</figcaption>
+        </figure>
+        <figure className="gallery-item">
+          <div className="gallery-item-wrapper">
+            <img src="/images/cr-bc.JPG" alt="" />
+          </div>
+          <figcaption>inner views</figcaption>
+        </figure>
+        <figure className="gallery-item">
+          <div className="gallery-item-wrapper">
+            <img src="/images/gw-bl.jpg" alt="" />
+          </div>
+          <figcaption>wines meeting</figcaption>
+        </figure>
+      </div>
+    </section>
+  );
 };
 
 export default Information;
